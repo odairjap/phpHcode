57563284f082a365eb66665b9fae7397b3d80d95
odairjap <odairjap@gmail.com>
2023-10-08 21:17:09 -0300

    concatenação de variáveis

--- variaveis/ex01.php
+++ variaveis/ex01.php
@@ -1,8 +1,15 @@
 <?php
-$nome = "Hcode";
+$nome = "Odair";
+$sobrenome = "Santos"; 
+$nomeCompleto = $nome . " " . $sobrenome;
 
 echo $nome;
+echo " ";
+echo $sobrenome;
+echo "<br/>";
+echo $nomeCompleto;
 
 var_dump($nome);
-
+var_dump($sobrenome);
+var_dump($nomeCompleto);
 ?>
