ae012f8f22c87f2d7b0765d557d1d53b45486734
odairjap <odairjap@gmail.com>
2023-10-14 13:02:41 -0300

    exercício de variaveis

--- variaveis/ex03.php
+++ variaveis/ex03.php
@@ -1,4 +1,29 @@
 <?php
+$empresa = "Hcode";
+$site = 'www.hcode.com.br';
+$ano = 1990;
+$salario = 5499.99;
+$bloqueado = false;
+/////////////////////////
 
+$frutas = array("abacaxi", "laranja", "manga");
+echo $frutas [2];
+$nascimento = new Datetime();
+/////////////////////////
+$arquivo = fopen("ex01.php", "r");
+////////////////////
+$nulo = NULL;
+$vazio = "";
 
+///////////////////////
+var_dump($empresa);
+var_dump($site);
+var_dump($ano);
+var_dump($salario);
+var_dump($bloqueado);
+var_dump($frutas);
+var_dump($nascimento);
+var_dump($arquivo);
+var_dump($nulo);
+var_dump($vazio);
 ?>
